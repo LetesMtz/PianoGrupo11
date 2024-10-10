e4b3bf8faec1fc0d7175cbca4cf1a2e5877ce478
Kevin Quiñonez <kquinonez@gmail.com>
2024-10-10 14:52:42 -0600

    Actualiza

--- app/src/main/java/sv/edu/catolica/pianogrupo11/MainActivity.java
+++ app/src/main/java/sv/edu/catolica/pianogrupo11/MainActivity.java
@@ -1,10 +1,13 @@
 package sv.edu.catolica.pianogrupo11;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,17 +18,139 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton ButtonDrawerToggle,ButtonDrawerToggle1,ButtonDrawerToggle2,ButtonDrawerToggle3,ButtonDrawerToggle4,ButtonDrawerToggle5,ButtonDrawerToggle6;
+
+    View dcorview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        dcorview = findViewById(R.id.main);
+        dcorview.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ButtonDrawerToggle = findViewById(R.id.buttondrawertoggle2);
+        ButtonDrawerToggle1 = findViewById(R.id.buttondrawertoggle1);
+        ButtonDrawerToggle2 = findViewById(R.id.buttondrawertoggle3);
+        ButtonDrawerToggle3 = findViewById(R.id.buttondrawertoggle4);
+        ButtonDrawerToggle4 = findViewById(R.id.buttondrawertoggle5);
+        ButtonDrawerToggle5 = findViewById(R.id.buttondrawertoggle6);
+        ButtonDrawerToggle6 = findViewById(R.id.buttondrawertoggle7);
+
+        ButtonDrawerToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notado();
+            }
+
+        });
+
+        ButtonDrawerToggle1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notare();
+            }
+        });
+
+        ButtonDrawerToggle5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notami();
+            }
+        });
+
+        ButtonDrawerToggle2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notafa();
+            }
+        });
+
+        ButtonDrawerToggle6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notasol();
+            }
+        });
+
+        ButtonDrawerToggle3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notala();
+            }
+        });
+
+        ButtonDrawerToggle4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notasi();
+            }
+        });
     }
+
+    private void notasol() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.dor);
+
+        sonidore.start();
+    }
+
+    private void notami() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.re);
+
+        sonidore.start();
+    }
+
+    private void notasi() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.mi);
+
+        sonidore.start();
+    }
+
+    private void notala() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.fa);
+
+        sonidore.start();
+    }
+
+    private void notafa() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.sol);
+
+        sonidore.start();
+    }
+
+    private void notare() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.la);
+
+        sonidore.start();
+    }
+
+    private void notado() {
+
+        MediaPlayer sonidoDo = MediaPlayer.create(this,R.raw.si);
+
+        sonidoDo.start();
+    };
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/sv/edu/catolica/pianogrupo11/PianoInfantilSelva.java
+++ app/src/main/java/sv/edu/catolica/pianogrupo11/PianoInfantilSelva.java
@@ -1,10 +1,13 @@
 package sv.edu.catolica.pianogrupo11;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,17 +17,136 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class PianoInfantilSelva extends AppCompatActivity {
 
+    ImageButton ButtonDrawerToggle,ButtonDrawerToggle1,ButtonDrawerToggle2,ButtonDrawerToggle3,ButtonDrawerToggle4,ButtonDrawerToggle5,ButtonDrawerToggle6;
+
+    View dcorview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_piano_infantil_selva);
+        dcorview = findViewById(R.id.main);
+        dcorview.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ButtonDrawerToggle = findViewById(R.id.buttondrawertoggle2);
+        ButtonDrawerToggle1 = findViewById(R.id.buttondrawertoggle1);
+        ButtonDrawerToggle2 = findViewById(R.id.buttondrawertoggle3);
+        ButtonDrawerToggle3 = findViewById(R.id.buttondrawertoggle4);
+        ButtonDrawerToggle4 = findViewById(R.id.buttondrawertoggle5);
+        ButtonDrawerToggle5 = findViewById(R.id.buttondrawertoggle6);
+        ButtonDrawerToggle6 = findViewById(R.id.buttondrawertoggle7);
+
+        ButtonDrawerToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notado();
+            }
+
+        });
+
+        ButtonDrawerToggle1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notare();
+            }
+        });
+
+        ButtonDrawerToggle5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notami();
+            }
+        });
+
+        ButtonDrawerToggle2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notafa();
+            }
+        });
+
+        ButtonDrawerToggle6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notasol();
+            }
+        });
+
+        ButtonDrawerToggle3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notala();
+            }
+        });
+
+        ButtonDrawerToggle4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notasi();
+            }
+        });
     }
+
+    private void notasol() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.dor);
+
+        sonidore.start();
+    }
+
+    private void notami() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.re);
+
+        sonidore.start();
+    }
+
+    private void notasi() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.mi);
+
+        sonidore.start();
+    }
+
+    private void notala() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.fa);
+
+        sonidore.start();
+    }
+
+    private void notafa() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.sol);
+
+        sonidore.start();
+    }
+
+    private void notare() {
+
+        MediaPlayer sonidore = MediaPlayer.create(this,R.raw.la);
+
+        sonidore.start();
+    }
+
+    private void notado() {
+
+        MediaPlayer sonidoDo = MediaPlayer.create(this,R.raw.si);
+
+        sonidoDo.start();
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,8 +159,8 @@ public class PianoInfantilSelva extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if(id == R.id.piano_instrumentos){
-            Intent pantalla = new Intent(PianoInfantilSelva.this, PianoInstrumentosMusicales.class);
+        if(id == R.id.piano_selva){
+            Intent pantalla = new Intent(PianoInfantilSelva.this, PianoInfantilSelva.class);
             startActivity(pantalla);
             finish();
         }else if(id == R.id.piano_tradicional){
@@ -53,4 +175,8 @@ public class PianoInfantilSelva extends AppCompatActivity {
 
         return true;
     }
+
 }
+
+
+
